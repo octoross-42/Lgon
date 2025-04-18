4d653da782ac7fde01ae86eea61a5696d45a281a
octoross-42 <codingcoeane@gmail.com>
2025-04-18 02:47:14 +0200

    Feat(types and main): debut transformation en ts avec ajout interfaces des Commands

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { Client, Collection, Command } from "discord.js";
+
+require('dotenv').config();
+const bot:Client = new Client();
+
+function initBotCollections(bot: Client) {
+	const collections = {
+	  commands: new Collection<string, Command>(),
+	  cooldowns: new Collection<string, number>(),
+	};
+	
+	Object.assign(bot, collections);
+  }
+
+initBotCollections(bot);
+	// "commands",
+	// "cooldowns",
+	// "parties",
+	// "channelsJeu",
+	// "messagesRoles",
+	// "rolesCommands",
+	// "joueurs",
+	// "joueur"
+
+// joueur : author.id -> joueurs
+// joueurs : guild.id -> joueurs
+// parties : guild.id -> partie
+
+//https://www.youtube.com/watch?v=QkbveDG94ik&list=PLuWyq_EO5_ALOnpxptlqQA5FR75Nza2PQ&index=12
+
+// loadCommands(bot);
+// loadEvents(bot);
+// loadRoles(bot);
+
+bot.login(process.env.TOKEN);
+
+console.log(`Bot is online !`);
---
+++ src/types/discord.d.ts
@@ -0,0 +1,23 @@
+declare module 'discord.js'
+{
+	import { Client, Collection, Message } from 'discord.js';
+	
+	export interface Command
+	{
+		help: {
+			name: string;
+			aliases?: string[];
+			category?: string;
+			description: string;
+			nbrArgsRequired: number;
+			cooldown: number; // en secondes
+			usage: string;
+		};
+		run: (bot:Client, message: Message, argv: string[]) => Promise<void> | void;
+	}
+  
+	interface Client
+	{
+	  commands: Collection<string, Command>;
+  }
+}
