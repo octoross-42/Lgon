4336bac4f9ac08580b68dba9240a7880eeb8a3ba
octoross-42 <codingcoeane@gmail.com>
2025-04-19 03:54:43 +0200

    Structure (bot_events/message.ts): dissection code en petit morceau de verification du message et de la commande demandee

--- src/bot_events/message.ts
+++ src/bot_events/message.ts
@@ -1,52 +1,88 @@
-import { Client, Message } from 'discord.js';
+import { Client, Message, Collection, Command } from 'discord.js';
 import { CONSTANTES } from '../config/constantes';
 
 // https://www.youtube.com/watch?v=O6S8p5rqQjc&list=PLuWyq_EO5_ALOnpxptlqQA5FR75Nza2PQ&index=13
 
-module.exports = (bot: Client, message: Message) =>
+function getArgv(message: Message): string[] | null
 {
-    if ( message.author.bot )
-		return;
-
 	const argv: string[] = message.content.split(/ +/);
 	if (argv.length < 2)
-		return;
+		return (null);
 	if (argv[0].toLowerCase() !== CONSTANTES.PREFIX)
-		return;
+		return (null);
+	argv.shift(); // remove PREFIX
+	return (argv);
+}
 
-    const commandName:string = argv[1].toLowerCase();
+function getCommand(bot: Client, argv: string[]): Command | null
+{
+    const commandName:string = argv[0].toLowerCase();
 
-
-    const command = bot.commands.get(commandName) || bot.commands.find(cmd => !!cmd.help.aliases?.includes(commandName));
+    const command = bot.commands.get(commandName)
+						|| bot.commands.find(cmd => !!cmd.help.aliases?.includes(commandName));
     if (!command)
-		return;
+		return (null);
+
+    if (argv.length - 1 < command.help.nbrArgsRequired)
+	{
+		// send help pour la commande
+		return (null);
+	}
+	return (command);
+}
+
+function isOnCooldown(bot: Client, command: Command, message: Message): boolean
+{
+ if (!bot.cooldowns.has(command.help.name))
+        bot.cooldowns.set(command.help.name, new Collection());
+
+    const timeNowMs: number = Date.now();
+	
+	let commandUsers: Collection<string, number>;
+	if (!bot.cooldowns.has(command.help.name))
+	{
+		commandUsers = new Collection();
+		bot.cooldowns.set(command.help.name, commandUsers);
+	}
+    else
+		commandUsers = bot.cooldowns.get(command.help.name)!;
     
+	const cooldownCommandMs: number = command.help.cooldown * 1000;
 
-    // if (command.help.argv && argv.length < command.help.argv)
-    //     return message.channel.send("Pas assez d'arguments pour cette commande");
-
-    // if (!bot.cooldowns.has(command.help.name))
-    //     bot.cooldowns.set(command.help.name, new Collection());
-
-    // const timeNow = Date.now();
-    // const tUtilisateurs = bot.cooldowns.get(command.help.name);
-    // const cooldownCommand = (command.help.cooldown ) * 1000;
-
-    // if (tUtilisateurs.has(message.author.id))
-	// {
-    //     const cdExpirationTime = tUtilisateurs.get(message.author.id) + cooldownCommand;
+    if (commandUsers.has(message.author.id))
+	{
+        const endCooldownTimeMs: number = commandUsers.get(message.author.id)! + cooldownCommandMs;
         
-    //     if (timeNow < cdExpirationTime)
-	// 	{
-    //         timeLeft = (cdExpirationTime - timeNow) / 1000;
-    //         return message.reply(` Cooldown restant pour \`${command.help.name}\` pour l'utilisateur \`${message.author.tag}\` : ${timeLeft.toFixed(0)} secondes`);
-    //     }
-    // }
+        if (timeNowMs < endCooldownTimeMs)
+		{
+			const timeLeftSec: number = (endCooldownTimeMs - timeNowMs) / 1000;
+            message.reply(` Cooldown restant pour \`${command.help.name}\` pour l'utilisateur \`${message.author.tag}\` : ${timeLeftSec.toFixed(0)} secondes`);
+	
+			return (true);
+        }
+    }
 
-    // tUtilisateurs.set(message.author.id, timeNow);
-    
-    // setTimeout(() => tUtilisateurs.delete(message.author.id), cooldownCommand);
+    commandUsers.set(message.author.id, timeNowMs);
+    setTimeout(() => commandUsers.delete(message.author.id), cooldownCommandMs);
 
+	return (false);
+}
+
+module.exports = (bot: Client, message: Message): Promise<void> | void =>
+{
+	if ( message.author.bot )
+		return ;
+
+	let argv: string[] | null = getArgv(message);
+	if ( !argv )
+		return ;
+
+	const command: Command | null = getCommand(bot, argv);
+	if ( !command )
+		return ;
+
+	if (isOnCooldown(bot, command, message))
+		return ;
     
     // if (command.help.prive && message.guild) return message.channel.send( `La commande \`${CONSTANTES.PREFIX}${command.help.name}\` doit être envoyée en privé`);
     // if (command.help.serveur && !message.guild) return message.channel.send( `La commande \`${CONSTANTES.PREFIX}${command.help.name}\` doit être envoyée dans un serveur`);
@@ -59,6 +95,6 @@ module.exports = (bot: Client, message: Message) =>
 	// 	{
     //         return message.channel.send(`Veuillez configurer les channels de Jeu avec \`${CONSTANTES.PREFIX} get_ready\` ou pour changer de channel : \`${CONSTANTES.PREFIX}set_channel\``); 
     //     }
-    // } 
+    // }
     // command.run(bot, message, argv);
 }
