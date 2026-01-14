39203d4f80aa190cff28fcc5ebf14505def43534
octoross-42 <codingcoeane@gmail.com>
2026-01-14 20:40:46 +0100

    fix(LgonRole-class): changes constructor args and rm unused values
    Changed how roles get generated with the class LgonRoleGenerator, so changes the constructor to LgonRoleGenerator arg instead of just LgonRoleHelp (also now Help is Meta)

--- src/classes/LgonRole/LgonRole.ts
+++ src/classes/LgonRole/LgonRole.ts
@@ -1,19 +1,11 @@
 import { Player } from "../Game/Player.js";
-import { LgonRoleHelp, Client } from "discord.js";
+import { LgonRoleGenerator } from "./LgonRoleGenerator.js";
+import { Client } from "discord.js";
 
 export abstract class LgonRole
 {
-	// this.inGameId = -1;
-	// this.rolesToKill = [];
-	// this.playerToKill = [];
-	// this.rolesToKeepAlive = [];
-	// this.playersToKeepAlive = [];
-	// this.run = run;
-	
 	id: number;
 	owner: Player | string;
-	printName: string;
-	help: LgonRoleHelp;
 	// rolesToKill: number[];
 	// playerToKill: string[];
 	// rolesToKeepAlive: number[];
@@ -21,17 +13,16 @@ export abstract class LgonRole
 	
 		// run: (bot: Client, message: Message, argv: string[]) => Promise<void> | void;
 	
-	constructor(help: LgonRoleHelp, printName: string, owner: Player | string, id: number)
+	constructor(public readonly generator: LgonRoleGenerator,
+			owner: Player | string,
+			id: number)
 	{
-		this.help = help;
-		this.printName = printName;
 		this.owner = owner;
 		this.id = id;
 	}
 
 	async preshot_action(bot: Client): Promise<void> {}
 	register_action(bot: Client, actions: string[]): void {}
-
-	async play_auto() {}
+	play_auto(): void {}
 
 }
