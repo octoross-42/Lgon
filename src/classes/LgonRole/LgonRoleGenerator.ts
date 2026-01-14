42184b504b3dfb2cd70dd0a129a340382f1519e2
octoross-42 <codingcoeane@gmail.com>
2026-01-14 20:41:57 +0100

    fix(LgonRoleGenerator): Changed role generation method

--- src/classes/LgonRole/LgonRoleGenerator.ts
+++ src/classes/LgonRole/LgonRoleGenerator.ts
@@ -1,4 +1,4 @@
-import type { LgonRoleHelp, Client } from "discord.js";
+import type { LgonRoleMeta, Client } from "discord.js";
 import { LgonRole } from "./LgonRole.js"
 import { Player } from "../Game/Player.js";
 
@@ -23,48 +23,22 @@ export function getRole(bot: Client, roleName: string): LgonRoleGenerator | null
 
 export class LgonRoleGenerator
 {
-	name: string;
-	printName: string;
-	description: string;
-	aliases: string[];
-	category: "Loup" | "Villageois" | "Independant";
-	cdv: string;
-	usage: string;
-	action: boolean;
-	information: boolean;
-	vote: boolean;
-	roleGenerator: <R extends LgonRole>(help : LgonRoleHelp, printName: string, owner: Player | string, id: number) => R;
+	public readonly printName: string;
+	declare public readonly name : string;
+	declare public readonly category: "Loup" | "Villageois" | "Independant";
+	declare public readonly description: string;
+	declare public readonly cdv: string;
+	declare public readonly usage: string;
+	declare public readonly aliases: string[];
+	declare public readonly action: boolean;
+	declare public readonly information: boolean;
+	declare public readonly vote: boolean;
 
-	constructor(help : LgonRoleHelp, roleGenerator: <R extends LgonRole>(help : LgonRoleHelp, printName: string, owner: Player | string, id: number) => R)
+	constructor(meta : LgonRoleMeta,
+				private readonly roleClass: new (generator: LgonRoleGenerator,owner: Player | string, id: number) => LgonRole)
 	{
-		this.name = help.name;
-		this.printName = this.name[0].toUpperCase() + this.name.substring(1);
-		this.description = help.description;
-		this.usage = help.usage;
-		this.aliases = help.aliases || [];
-		this.cdv = help.cdv;
-		this.action = help.action;
-		this.information = help.information;
-		this.vote = help.vote;
-
-		this.category = help.category;
-		this.roleGenerator = roleGenerator;
-		console.log(this.category);
-		
+		Object.assign(this, meta);
+		this.printName = this.name[0].toUpperCase() + this.name.slice(1);
 	}
-
-	generateRole<R extends LgonRole>(owner: Player | string, id: number): R
-	{
-		let help: LgonRoleHelp = {
-			name: this.name,
-			category: this.category,
-			description: this.description,
-			cdv: this.cdv,
-			usage: this.usage,
-			action: this.action,
-			information: this.information,
-			vote: this.vote
-		};
-		return (this.roleGenerator(help, this.printName, owner, id));
-	}
+	generateRole(owner: Player | string, id: number): LgonRole { return new this.roleClass(this, owner , id); }
 }
