59ab5b537746659ff4ffbe6a1d4402423d7252fa
octoross-42 <codingcoeane@gmail.com>
2026-01-14 21:03:39 +0100

    test(main): added some error handling in // to test bug (client disconnects and quits) now fixed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import { loader } from "./loader/loader.js"
 
 import 'dotenv/config';
 
+
+// import dns from "node:dns";
+// dns.setDefaultResultOrder("ipv4first");
+
+
+// process.on("unhandledRejection", (reason) => {
+//   console.error("UNHANDLED REJECTION:", reason);
+// });
+
+// process.on("uncaughtException", (err) => {
+//   console.error("UNCAUGHT EXCEPTION:", err);
+// });
+
+
 const bot:Client = new Client( {intents: [
     GatewayIntentBits.Guilds, // Intent de base pour les serveurs
     GatewayIntentBits.GuildMessages, // Pour les messages des salons textuels
@@ -15,5 +29,10 @@ const bot:Client = new Client( {intents: [
 ]});
 
 await loader(bot);
+
+
+// bot.on("error", (err) => console.error("DISCORD CLIENT ERROR:", err));
+// bot.on("shardError", (err) => console.error("SHARD ERROR:", err));
+
 
 bot.login(process.env.TOKEN);
